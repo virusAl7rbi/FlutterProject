d2f1d052aec6b1653719c59f979801b66b36e778
virusal7rbi <fahadalwasidi@gmail.com>
2022-09-15 11:03:37 +0300

    ui updates

--- lib/course.dart
+++ lib/course.dart
@@ -7,14 +7,14 @@ class Course {
   final String description;
   final String trainer;
 
-  Course(@required this.title, @required this.trainer, this.description);
+  Course(this.title, this.trainer, this.description);
 }
 
 class CourseDet extends StatelessWidget {
-  final title;
-  final trainer;
-  final description;
-  const CourseDet(this.title, this.trainer, this.description);
+  final String title;
+  final String trainer;
+  final String description;
+  const CourseDet({super.key, required this.title, required this.description, required this.trainer});
 
   @override
   Widget build(BuildContext context) {
@@ -67,3 +67,74 @@ class CourseDet extends StatelessWidget {
     );
   }
 }
+
+
+// class Instructor {
+//   final String name;
+//   final String courses;
+//   final String trainer;
+
+//   Course(this.title, this.trainer, this.description);
+// }
+
+// class InstructorDet extends StatelessWidget {
+//   final String name;
+//   final String trainer;
+//   final String description;
+//   const CourseDet(
+//       {super.key,
+//       required this.title,
+//       required this.description,
+//       required this.trainer});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       // width: 200,
+//       margin: EdgeInsets.only(top: 40, left: 15, right: 15),
+//       child: Container(
+//         decoration: BoxDecoration(
+//             color: Colors.white,
+//             boxShadow: [
+//               BoxShadow(
+//                 color: Colors.grey.withOpacity(0.5), //color of shadow
+//                 spreadRadius: 5, //spread radius
+//                 blurRadius: 7, // blur radius
+//                 offset: const Offset(0, 2), // changes position of shadow
+//               ),
+//               //you can set more BoxShadow() here
+//             ],
+//             borderRadius: BorderRadius.circular(10)),
+//         child: Padding(
+//           padding: const EdgeInsets.only(top: 10, left: 10),
+//           child:
+//               // ignore: duplicate_ignore
+//               Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+//             Text(
+//               title,
+//               style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
+//             ),
+//             // ignore: prefer_const_constructors
+//             SizedBox(
+//               height: 10,
+//             ),
+//             Text(
+//               "Trainer: $trainer",
+//               style: TextStyle(fontSize: 15, fontWeight: FontWeight.w600),
+//             ),
+//             // ignore: prefer_const_constructors
+//             SizedBox(
+//               height: 10,
+//             ),
+//             Expanded(
+//               child: SingleChildScrollView(
+//                 scrollDirection: Axis.vertical,
+//                 child: Text(description),
+//               ),
+//             )
+//           ]),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -2,30 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:project/course.dart';
-import 'package:project/profile.dart';
+import 'package:project/pages.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
-var courses = [
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-  Course("flutter", "unknown",
-      "In anim nulla magna magna culpa dolor cillum est. Est cillum ad ad aliquip sunt consectetur ea esse. Occaecat Lorem consequat ipsum aliquip excepteur sunt exercitation duis excepteur est commodo labore anim velit. Nisi incididunt aliqua dolor do. Culpa eiusmod dolore pariatur eu veniam irure eu."),
-];
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -46,29 +29,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  // ignore: prefer_final_fields
   List<Widget> _pages = [
-    Scaffold(
-      appBar: AppBar(
-        title: Text("courses"),
-      ),
-      body: GridView(
-        shrinkWrap: true,
-        // ignore: sort_child_properties_last
-        children: courses
-            .map((course) =>
-                CourseDet(course.title, course.trainer, course.description))
-            .toList(),
-        gridDelegate:
-            SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 220),
-      ),
-    ),
-    Scaffold(
-        appBar: AppBar(
-          title: Text("instructors"),
-        ),
-        body: TabBarView(
-          children: <Widget>[],
-        )),
+    CoursesPage(),
+    InstructorPage(),
+    ProfilePage()
   ];
 
   int _selectedPageIndex = 0;
@@ -84,41 +49,6 @@ class _HomePageState extends State<HomePage> {
     return DefaultTabController(
         length: 0,
         child: Scaffold(
-          appBar: AppBar(
-            title: const Text(
-              "main screen",
-              style: TextStyle(fontSize: 25),
-            ),
-          ),
-          drawer: Drawer(
-            child: SafeArea(
-              child: ListView(
-                children: [
-                  Column(
-                    children: [
-                      SizedBox(
-                        height: 150,
-                        child: Image.asset("assets/img/logo.png"),
-                      ),
-                      SizedBox(
-                        width: 200,
-                        child: ElevatedButton.icon(
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ProfilePage()));
-                          },
-                          icon: const Icon(Icons.account_circle_outlined),
-                          label: const Text("Profile"),
-                        ),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
           body: _pages[_selectedPageIndex],
           bottomNavigationBar: BottomNavigationBar(
               backgroundColor: Colors.blue,
@@ -126,11 +56,18 @@ class _HomePageState extends State<HomePage> {
               unselectedItemColor: Colors.white,
               currentIndex: _selectedPageIndex,
               onTap: _x1,
+              // ignore: prefer_const_literals_to_create_immutables
               items: [
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.account_tree_sharp), label: "courses"),
+                    icon: Icon(Icons.play_circle_fill), label: "courses"),
                 BottomNavigationBarItem(
-                    icon: Icon(Icons.account_tree_sharp), label: "instructors")
+                    icon: Icon(Icons.supervised_user_circle_rounded),
+                    label: "instructors"),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.account_circle_rounded,
+                    ),
+                    label: "Profile")
               ]),
         ));
     //Column
